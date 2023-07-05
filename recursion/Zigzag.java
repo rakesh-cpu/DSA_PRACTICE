a09ae77b796a8d32669e1eb8cbf9f7f357577fc4
rakeshdontula06 <rakeshdontula66@gmail.com>
2023-07-05 22:49:05 +0530

    new changes

--- recursion/Zigzag.java
+++ recursion/Zigzag.java
@@ -3,19 +3,21 @@ package recursion;
 public class Zigzag{
     public static void printzigzag(int n){
         if(n==0){
-            return;
+            return ;
         }
         System.out.println("pre->"+n);
         printzigzag(n-1);
         System.out.println("in->"+n);
         printzigzag(n-1);
         System.out.println("post->"+n);
+    
 
 
     }
     public static void main(String[] args) {
-        int n = 2;
-        printzigzag(n);
+        int n = 3;
+         printzigzag(n);
+        
 
     }
 }
