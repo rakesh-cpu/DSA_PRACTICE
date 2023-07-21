39f7ebbff6c9b756e54115167dea3aaf60152515
rakeshdontula06 <rakeshdontula66@gmail.com>
2023-07-21 22:15:28 +0530

    code3 done

---
+++ Accenture/code3.java
@@ -0,0 +1,31 @@
+package Accenture;
+import java.util.*;
+public class code3 {
+
+    public static int findAbsoluteMinDif(int[] arr,int value){
+        int min = Math.abs(arr[0]-value);
+        int idx = 0;
+        for(int i=1;i<arr.length;i++){
+            if(min>Math.abs(arr[i]-value)){
+                idx = i;
+            }
+            min = Math.min(min,Math.abs(arr[i]-value));
+               
+        }
+
+
+        return arr[idx];
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n =sc.nextInt();
+        int arr[] = new int[n];
+        for(int i=0;i<arr.length;i++){
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("enter the number to be checked with:");
+        int value = sc.nextInt();
+        int res = findAbsoluteMinDif(arr,value);
+        System.out.println(res);
+    }
+}
