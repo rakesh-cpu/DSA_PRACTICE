92b6282ccb1bd46dfad164d905d84a89f7cacc8d
rakeshdontula06 <rakeshdontula66@gmail.com>
2023-07-25 23:18:48 +0530

    checked hashcode of @

--- Accenture/code4.java
+++ Accenture/code4.java
@@ -3,5 +3,7 @@ package Accenture;
 public class code4 {
     public static void main(String[] args) {
         System.out.println("3"+3);
+        String str = "@";
+        System.out.println(str.hashCode());
     }
 }
