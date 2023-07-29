c560138c1c97e5789d2303efa7f4904f1b8226cc
rakeshdontula06 <rakeshdontula66@gmail.com>
2023-07-29 22:04:01 +0530

    commited

--- Accenture/code3.java
+++ Accenture/code3.java
@@ -12,6 +12,7 @@ public class code3 {
             min = Math.min(min,Math.abs(arr[i]-value));
             System.out.println(min);
             System.out.println(idx);
+            System.out.println("sleeping");
                
         }
 
