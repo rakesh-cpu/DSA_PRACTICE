56f40c4602b1034b4bfbfdbe2ef5d536ea9bd0ee
rakeshdontula06 <rakeshdontula66@gmail.com>
2023-08-20 22:21:08 +0530

    todays push

--- Accenture/Code5.java
+++ Accenture/Code5.java
@@ -3,7 +3,9 @@ package Accenture;
 public class Code5{
     public static void main(String args[]){
         short x = 5;
-        x *= 5;
+        x = x*5;
+        int x = 10;
+        
         System.out.println(x);
     }
 }
