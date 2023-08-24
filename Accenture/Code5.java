d6b3afa70e4569cd1f5898fd57d0b177fc036468
rakeshdontula06 <rakeshdontula66@gmail.com>
2023-08-24 22:35:38 +0530

    todays push

--- Accenture/Code5.java
+++ Accenture/Code5.java
@@ -18,6 +18,11 @@ public class Code5{
         
         int x = 10;
         System.out.println(x);
+        System.out.println(x);
+        System.out.println(x);
+        System.out.println(x);
+        System.out.println(x);
+        System.out.println(x);
 
         
         int x = 10;
